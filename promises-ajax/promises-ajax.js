fa060c6585bfef2e02a0d59c4433d182b564cdd5
rid3thespiral <davidebattaglia1993@gmail.com>
2020-09-06 11:21:35 +0200

    promises resolution http response

--- promises-ajax/promises-ajax.js
+++ promises-ajax/promises-ajax.js
@@ -1,18 +1,29 @@
-window.onload = () => {
-    alert('loaded');
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', 'http://localhost:8000/index.php');
-    xhr.send();
-    xhr.onreadystatechange = () => {
-        if(xhr.readyState === 4 && xhr.status === 200){
-            let res = JSON.parse(xhr.responseText);
-            let ul = '<ul>'
-            ul+=res.map(book => '<li>'+book.title+'</li>');
-            document.querySelector('#content').innerHTML = ul;
+let $ = {
+    get : function(url){
+    let p = new Promise((resolve,reject)=>{
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', url);
+        xhr.send();
+        xhr.onreadystatechange = () => {
+            if(xhr.readyState === 4 && xhr.status === 200){
+                resolve(xhr.responseText);
+            }
+        };
+        xhr.error = () => {
+            "use strict";
+            reject("Error contacting server")
         }
-    };
-    xhr.error = () => {
-        "use strict";
-        alert("Problem contacting server");
+    })
+    return p;
     }
+}
+
+window.onload = () => {
+    $.get('/index.php').then(res => {
+        "use strict";
+        let obj = JSON.parse(res);
+        let ul = '<ul>'
+        ul+=obj.map(book => '<li>'+book.title+'</li>');
+        document.querySelector('#content').innerHTML = ul;
+    }).catch(error => console.log(error))
 }
